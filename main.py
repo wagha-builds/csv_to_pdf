09cd1c65235bdff1891b12cb2c5f80631076791c
wagha-builds <arwaghmode09@gmail.com>
2025-09-06 21:07:41 +0530

    From CSV to PDF

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 from fpdf import FPDF
+import pandas as pd
 
 #Orientation is either Portrait or Landscape.
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
-pdf.add_page()
+df = pd.read_csv("topics.csv")
+for index, row in df.iterrows():
+    pdf.add_page()
 
-pdf.set_font(family="Times", style="B", size=12)
-pdf.cell(w=0, h=12, txt="Hello World!", align="L", ln=1, border=1)
-pdf.set_font(family="Times", style="B", size=10)
-pdf.cell(w=0, h=10, txt="Hi World!", align="L", ln=1, border=1)
-
-pdf.add_page()
-
-pdf.set_font(family="Times", style="B", size=12)
-pdf.cell(w=0, h=12, txt="Hello World!", align="L", ln=1)
-pdf.set_font(family="Times", size=10)
-pdf.cell(w=0, h=12, txt="Hi World!", align="L", ln=1)
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+    pdf.line(10, 21, 200, 21)
 
 pdf.output("output.pdf")
